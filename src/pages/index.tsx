7f1a6f858883b5f643d26ce282878fa76769a921
erick <erick_ic@163.com>
2025-05-24 16:09:44 +0800

    hello world

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+
+export default function HomePage() {
+  return (
+    <div >
+      <span style={{color:"black", fontSize:"21px"}}>hello</span>
+      &nbsp;
+      &nbsp;
+      <span style={{color:"orange", fontSize:"21px"}}>world~</span>
+    </div>
+  );
+}
